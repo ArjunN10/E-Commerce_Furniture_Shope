28ea2a67e163dbde66dbfdfc1f0c867b7974da2c
ARJUN N <138310139+ArjunN10@users.noreply.github.com>
2023-09-16 22:00:53 +0530

    fourth commit

--- src/components/products/Search.js
+++ src/components/products/Search.js
@@ -1,43 +1,55 @@
-import React, { useContext, useState } from 'react';
+import React, { useContext, useState } from "react";
 
-import { MDBDropdown, MDBDropdownToggle, MDBDropdownMenu, MDBDropdownItem, MDBInput, MDBBtn, MDBIcon } from 'mdb-react-ui-kit';
-import { useNavigate } from 'react-router-dom';
-import { Mycontext } from '../../context/Context';
+import {
+  MDBDropdown,
+  MDBDropdownToggle,
+  MDBDropdownMenu,
+  MDBDropdownItem,
+  MDBInput,
+  MDBBtn,
+  MDBIcon,
+} from "mdb-react-ui-kit";
+import { useNavigate } from "react-router-dom";
+import { Mycontext } from "../../context/Context";
 
 const Search = () => {
-  const  {productsList } = useContext(Mycontext);
-  const [searchQuery, setSearchQuery] = useState('');
-  const ab = useNavigate();
+  const { productsList } = useContext(Mycontext);
+  const [searchQuery, setSearchQuery] = useState("");
+  const navigate = useNavigate();
 
   const handleSearchChange = (e) => {
     setSearchQuery(e.target.value);
   };
 
-  const filteredProducts =productsList.filter((item) =>
+  const filteredProducts = productsList.filter((item) =>
     item.name.toLowerCase().includes(searchQuery.toLowerCase())
   );
 
   return (
     <div>
       <MDBDropdown>
-        <MDBDropdownToggle className="shadow-none bg-transparent border-0 overflow-hidden" >
+        <MDBDropdownToggle className="shadow-none bg-transparent border-0 overflow-hidden">
           <form className="d-flex">
             <MDBInput
-              type="search"
-              placeholder="Search"
+              type="search "
+              placeholder="Search Here"
               className="me-2 my-2"
               value={searchQuery}
               onChange={handleSearchChange}
             />
-            <MDBBtn outline color="success" className="my-1 my-2">
-              <MDBIcon icon="search" />
+            <MDBBtn outline color="success"  className="my-1 my-1">
+              <MDBIcon icon="search"/>
             </MDBBtn>
           </form>
         </MDBDropdownToggle>
         <MDBDropdownMenu end className="overflow-hidden">
           {filteredProducts.map((item) => (
-            <MDBDropdownItem key={item.id} onClick={() => ab(`/productview/${item.id}`)}>
-              <img src={item.src} className="w-25" alt={item.name} /> {item.name}
+            <MDBDropdownItem
+              key={item.id}
+              onClick={() => navigate(`/productview/${item.id}`)}
+            >
+              <img src={item.src} className="w-25" alt={item.name} />{" "}
+              {item.name}
             </MDBDropdownItem>
           ))}
         </MDBDropdownMenu>
