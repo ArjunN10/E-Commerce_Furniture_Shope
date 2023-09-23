40748ece5fdc836fd0dffcd199d08b0cd3deca77
ARJUN N <138310139+ArjunN10@users.noreply.github.com>
2023-09-23 22:50:53 +0530

    sixth  commit

--- src/LoginSignup/Signup.js
+++ src/LoginSignup/Signup.js
@@ -27,9 +27,11 @@ function Signup() {
     const repassword = event.target.repwd.value;
     const emailvalid = user.filter((item) => item.email === email);
     if (emailvalid.length !== 0) {
-      alert("Email already exist");
+      alert("Email already exists");
     } else {
-      setuser([...user,{
+      setuser([
+        ...user,
+        {
           name: name,
           email: email,
           password: password,
@@ -37,15 +39,18 @@ function Signup() {
         },
       ]);
     }
-    // console.log(name);
     event.target.reset();
   };
-  // console.log(user);
 
   return (
     <MDBContainer fluid>
-<MDBCard className="text-black m-5" style={{ borderRadius: "25px", boxShadow: "rgb(38, 57, 77) 0px 20px 30px -10px" }}>
-
+      <MDBCard
+        className="text-black m-3 m-md-5"
+        style={{
+          borderRadius: "25px",
+          boxShadow: "rgb(38, 57, 77) 0px 20px 30px -10px",
+        }}
+      >
         <MDBCardBody>
           <MDBRow>
             <MDBCol
@@ -53,60 +58,64 @@ function Signup() {
               lg="6"
               className="order-2 order-lg-1 d-flex flex-column align-items-center"
             >
-              <form className='mt-5'onSubmit={register}>
-                <h3 classNAme="text-center h1 fw-bold mb-5 mx-1 mx-md-4  ">
+              <form onSubmit={register}>
+                <h3 className="text-center h1 fw-bold mb-5 mx-1 mx-md-4">
                   Sign up
                 </h3>
 
-                <div className="d-flex flex-row align-items-center mb-4 ">
-                  <MDBIcon fas icon="user me-3" size="lg" />
-                  <MDBInput
-                    label="Your Name"
-                    id="name"
-                    type="text"
-                    className="w-100"
-                    required
-                  />
+                <div className="mb-4">
+                  <div className="d-flex flex-row align-items-center mb-3">
+                    <MDBIcon fas icon="user me-3" size="lg" />
+                    <MDBInput
+                      label="Your Name"
+                      id="name"
+                      type="text"
+                      className="w-100"
+                      required
+                    />
+                  </div>
+
+                  <div className="d-flex flex-row align-items-center mb-3">
+                    <MDBIcon fas icon="envelope me-3" size="lg" />
+                    <MDBInput
+                      label="Your Email"
+                      id="email"
+                      type="email"
+                      required
+                    />
+                  </div>
+
+                  <div className="d-flex flex-row align-items-center mb-3">
+                    <MDBIcon fas icon="lock me-3" size="lg" />
+                    <MDBInput
+                      label="Password"
+                      id="pwd"
+                      type="password"
+                      required
+                    />
+                  </div>
+
+                  <div className="d-flex flex-row align-items-center mb-4">
+                    <MDBIcon fas icon="key me-3" size="lg" />
+                    <MDBInput
+                      label="Repeat your password"
+                      id="repwd"
+                      type="password"
+                      required
+                    />
+                  </div>
                 </div>
 
-                <div className="d-flex flex-row align-items-center mb-4">
-                  <MDBIcon fas icon="envelope me-3" size="lg" />
-                  <MDBInput
-                    label="Your Email"
-                    id="email"
-                    type="email"
-                    required
-                  />
-                </div>
-
-                <div className="d-flex flex-row align-items-center mb-4">
-                  <MDBIcon fas icon="lock me-3" size="lg" />
-                  <MDBInput 
-                  label="Password" 
-                  id="pwd" 
-                  type="password" 
-                  required
-                  />
-                </div>
-
-                <div className="d-flex flex-row align-items-center mb-4">
-                  <MDBIcon fas icon="key me-3" size="lg" />
-                  <MDBInput
-                    label="Repeat your password"
-                    id="repwd"
-                    type="password"
-                    required
-                  />
-                </div>
                 <MDBBtn
-                 className="mb-4 ms-4" 
-                 size="lg" 
-                 onSubmit={()=>navigate('/login')}
-                 >
+                  className="mb-4 ms-4"
+                  size="lg"
+                  type="submit"
+                  onClick={() => navigate("/login")}
+                >
                   Register
                 </MDBBtn>
                 <p className="ms-4">
-                  or  <Link to={"/login"}><MDBBtn className="btn-primary">Login</MDBBtn></Link>
+                  or <Link to={"/login"}>Login</Link>
                 </p>
               </form>
             </MDBCol>
@@ -116,18 +125,19 @@ function Signup() {
               className="order-1 order-lg-2 d-flex align-items-center"
             >
               <MDBCardImage
-                 src={require("../components//products/images/cover15new.jpg")} 
-                 style={{ borderRadius: "20px" }}
+                src={require("../components/products/images/cover15new.jpg")}
+                style={{ borderRadius: "20px" }}
                 fluid
               />
             </MDBCol>
           </MDBRow>
         </MDBCardBody>
       </MDBCard>
-      <div className='mt-3'>
-    <Footer/>
-     </div>
+      <div className="mt-3">
+        <Footer />
+      </div>
     </MDBContainer>
   );
 }
+
 export default Signup;
