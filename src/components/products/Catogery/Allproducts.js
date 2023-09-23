fe66f2322ac085eca1c0ef309d43a14cd649878d
ARJUN N <138310139+ArjunN10@users.noreply.github.com>
2023-09-23 21:00:55 +0530

    first commit

--- src/components/products/Catogery/Allproducts.js
+++ src/components/products/Catogery/Allproducts.js
@@ -1,55 +1,48 @@
-import React, { useContext,useEffect } from 'react'
-import {MDBCard,MDBCardBody,MDBCardTitle,MDBCardText,MDBCardFooter,MDBCardImage,MDBBtn,MDBRipple} from 'mdb-react-ui-kit';
+import React, { useContext, useEffect } from 'react';
+import { MDBCard, MDBCardBody, MDBCardTitle, MDBCardText, MDBCardFooter, MDBCardImage, MDBBtn, MDBRipple } from 'mdb-react-ui-kit';
 import { Mycontext } from '../../../context/Context';
 import { useNavigate } from 'react-router-dom';
 import Nav from '../../Nav';
 import Footer from '../../Footer';
 
-
 function Allproducts() {
-    const {products}=useContext(Mycontext)
-    // console.log(productsList);
-    const navigate=useNavigate()
-    useEffect(() => {
-      window.scrollTo(0, 0);
-    }, []);
+  const { products } = useContext(Mycontext);
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    window.scrollTo(0, 0);
+  }, []);
 
   return (
     <>
-    <div  className=' sticky-top'>
-     <Nav/>
-    </div>
-    <div className='container mt-2 '>
-      <div className='row'>
-        {products.map((items)=>(
-            // <div className=''>
-<MDBCard key={products.id} className='w-25 mt-2'>
-      <MDBRipple rippleColor='light' rippleTag='div' className='bg-image hover-overlay'>
-        <MDBCardImage src={items.src} fluid alt='Photo' />
-        {/* <a>
-          <div className='mask' style={{ backgroundColor: 'rgba(251, 251, 251, 0.15)' }}></div>
-        </a> */}
-      </MDBRipple>
-      <MDBCardBody>
-        <MDBCardTitle>{items.name}</MDBCardTitle>
-        <MDBCardText>
-         {items.description}
-        </MDBCardText>
-      </MDBCardBody>
-      <MDBCardFooter className='my-auto mx-auto'>
-      <MDBBtn onClick={()=>navigate(`/productview/${items.id}`)}>view product</MDBBtn>
-
-          </MDBCardFooter>
-    </MDBCard>
-    //  </div>
-    ))}
-    </div>
-    </div>
-    <div className='mt-3'>
-      <Footer/>
-     </div>
+      <div className='sticky-top'>
+        <Nav />
+      </div>
+      <div className='container mt-2'>
+        <div className='row row-cols-1 row-cols-sm-2 row-cols-md-3'>
+          {products.map((items) => (
+            <div key={items.id} className='col mb-4'>
+              <MDBCard className='h-100'>
+                <MDBRipple rippleColor='light' rippleTag='div' className='bg-image hover-overlay'>
+                  <MDBCardImage src={items.src} fluid alt='Photo' />
+                </MDBRipple>
+                <MDBCardBody>
+                  <MDBCardTitle>{items.name}</MDBCardTitle>
+                  <MDBCardText>{items.description}</MDBCardText>
+                </MDBCardBody>
+                <MDBCardFooter className='my-auto mx-auto'>
+                  <MDBBtn onClick={() => navigate(`/productview/${items.id}`)}>View Product</MDBBtn>
+                </MDBCardFooter>
+              </MDBCard>
+            </div>
+          ))}
+        </div>
+      </div>
+      <div className='mt-3'>
+        <Footer />
+      </div>
     </>
-  )
+  );
 }
 
-export default Allproducts
+export default Allproducts;
